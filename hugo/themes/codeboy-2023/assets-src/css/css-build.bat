14376b513eb00ad1a63ba9e2b52e77b89ee10236
Don Parakin <150460994+donparakin@users.noreply.github.com>
2024-06-23 12:59:28 -0400

    Add PicoCSS-ified styling.

--- hugo/themes/codeboy-2023/assets-src/css/css-build.bat
+++ hugo/themes/codeboy-2023/assets-src/css/css-build.bat
@@ -3,27 +3,31 @@ rem ====================================================================
 rem build-css.bat:
 rem  - Call "sass" to build the CSS (optionally watching for changes)
 rem  - Param 1:  build|watch
+rem  - Param 2:  CSS file name (excluding .extension)
 rem ====================================================================
 setlocal EnableDelayedExpansion
 set "_action=%~1"
 
+set "_css=website"
 set "_src=themes/codeboy-2023/assets-src/css"
-set "_dest=themes/codeboy-2023/assets/css"
+set "_dest=assets/built"
 
-set "_src_dest=%_src%/website.scss:%_dest%/website.css"
+set "_src_dest=%_src%/%_css%.scss:%_dest%/%_css%.css"
 
-set "_opts1=--load-path=node_modules/@picocss/pico/scss"
-set "_opts1=%_opts% --stop-on-error"
-rem set "_opts1=%_opts% --style=expanded"
+set "_opts=--load-path=node_modules/@picocss/pico/scss"
+rem set "_opts=%_opts% --style=expanded"
 
 if "%_action%" == "build" (
-    set "_opts2=--quiet-deps"
+    set "_opts=%_opts% --stop-on-error"
+    set "_opts=%_opts% --quiet-deps"
+    set "_opts=%_opts% --style=compressed"
 )
 if "%_action%" == "watch" (
-    set "_opts2=--watch"
+    set "_opts=%_opts% --error-css"
+    set "_opts=%_opts% --watch"
 )
 
-sass %_src_dest% %_opts1% %_opts2%
+sass %_src_dest% %_opts%
 
 endlocal
 exit /B
